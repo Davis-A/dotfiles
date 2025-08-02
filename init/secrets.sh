dd564e666bc3990763196c65ab11d65bf51ac08e
Andrew Davis <adavis@fastmailteam.com>
2025-08-02 22:33:00 +1000

    secrets bootstrap script, also bootstrap host specific config
    This isn't secret, just things i don't want to go into a public git
    repo.

--- init/secrets.sh
+++ init/secrets.sh
@@ -3,14 +3,21 @@
 SECRETS_DIR="$HOME/.secrets"
 SECRETS_FILE="$SECRETS_DIR/chezmoi-secrets.toml"
 
-if [ -f $SECRETS_FILE ]; then
-  exit 0
+if [ ! -f $SECRETS_FILE ]; then
+  # it's fine if we're missing keys under .secrets
+  # because we set to ignore missing keys in chezmoi config
+  # The exception is the top level secrets key
+  mkdir -p $SECRETS_DIR
+  chmod 700 $SECRETS_DIR
+  echo '[secrets]' > $SECRETS_FILE
 fi
 
-
-# it's fine if we're missing keys under .secrets
-# because we set to ignore missing keys in chezmoi config
-# The exception is the top level secrets key
-mkdir -p $SECRETS_DIR
-chmod 700 $SECRETS_DIR
-echo '[secrets]' > $SECRETS_FILE
+# also setup a local.toml, it's not for secrets but i'm doing basically the same thing
+CHEZMOI_PATH=`chezmoi source-path`
+LOCAL_FILE="$CHEZMOI_PATH/.chezmoidata/local.toml"
+if [ ! -f $LOCAL_FILE ]; then
+  # it's fine if we're missing keys under .secrets
+  # because we set to ignore missing keys in chezmoi config
+  # The exception is the top level secrets key
+  echo '[local]' > $LOCAL_FILE
+fi
